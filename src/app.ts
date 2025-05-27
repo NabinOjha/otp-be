9e29eca83076f5a0ebe194bf8e1b7806da01e43e
Nabin Ojha <nabinojha47@gmail.com>
2025-05-27 16:58:59 +0545

    set up sentry

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ debug: true });
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('./sentry');
+
 import express from 'express';
+import Sentry from '@sentry/node';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
 import { errorHandler } from './middlewares/errorHandler';
 import authRoutes from './routes/auth';
@@ -12,7 +21,15 @@ app.use(cookieParser());
 
 app.use(express.json());
 
+app.use(morgan('dev'));
+
 app.use('/auth', authRoutes);
+
+app.get('/debug-sentry', function mainHandler() {
+  throw new Error('My first Sentry error!');
+});
+
+Sentry.setupExpressErrorHandler(app);
 
 app.use(errorHandler);
 export default app;
